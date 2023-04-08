d5ad7d6b7410a6608b3cac58c4b4e25eb8f32db2
Akrosha <akronobat@gmail.com>
2023-04-08 13:29:41 +0500

    maybe add de api :shrug:

--- host.py
+++ host.py
@@ -1,27 +1,41 @@
 from flask import Flask
 from threading import Thread
+from flask_restful import Api, Resource, reqparse
+from helpers.databaseFunctions import check_user, just_get_all
 
 app = Flask("")
+api = Api(app)
 
-
-@app.route('/')
+@app.route("/", methods = ["get"])
 def index():
     with open("webpages/index.html","r") as file:
         message = file.read()
     return message
 
-@app.route('/home/')
+@app.route("/home/", methods = ["get"])
 def home():
     with open("webpages/home.html","r") as file:
         message = file.read()
     return message
 
-@app.route("/help/")
+@app.route("/help/", methods = ["get"])
 def help():
     with open("webpages/help.html","r") as file:
         message = file.read()
     return message
 
+@app.route("/api/user/", methods = ["get"])
+def api_user():
+    player = just_get_all()
+    return {"status_code": 200, "data": player}
+
+@app.route("/api/user/<string:uid>/", methods = ["get"])
+def api_user_uid(uid: str):
+    player = check_user(uid)
+    if player:
+        return {"status_code": 200, "data": player}
+    else:
+        return {"status_code": 404}
 
 def run():
     app.run(host="0.0.0.0", port=8080)
